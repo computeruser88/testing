aff6824745c6efa0fdf5aa04693868f3f1ca1ad4
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-24 10:15:45 -0800

    Initial commit

---
+++ testing.js
@@ -0,0 +1,19 @@
+vafr whtevs = "a"
+console.log(whtevs ==="b")
+
+module:exports = {
+    assert: function(expected, actual, message = "Test") {
+        if (expected === actual) {
+            console.log(message);
+        } else {
+            diff = util.expect(expected) + "\n" + util.inspect(actual);
+            console.log(message + "\nAssertion failed no match\n" + diff);
+        }
+    },
+    assert_true: function (actual, message) {
+
+    },
+    assert_false: function (actual, message) {
+
+    }
+};
